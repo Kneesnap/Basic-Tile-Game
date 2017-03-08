4f4c57f13ba133966b19fbe653478dbe1f40317d
XxXxREExXxX <voiceless1848@gmail.com>
2017-03-08 15:23:08 -0800

    Added the beginning of obstacles if we implement this, welcome to AI hell

---
+++ TileGame/src/me/nadd/tilegame/tiles/ObstacleTile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.nadd.tilegame.tiles;
+
+import org.lwjgl.util.Color;
+import org.lwjgl.util.ReadableColor;
+
+/**
+ *
+ * @author voice
+ */
+public class ObstacleTile extends Tile {
+    
+    public ObstacleTile(int x, int y) {
+        super(x, y);
+    }
+    public boolean isWalkable(){
+        return false;
+    }
+    public ReadableColor getColor() {
+        return Color.DKGREY;
+    }
+}
