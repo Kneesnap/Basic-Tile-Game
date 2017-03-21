3aa18a44b93a17c2a39d864862234451b64940e6
Kneesnap <Kneesnap@users.noreply.github.com>
2017-03-21 08:34:02 -0700

    Update Utils.java

--- TileGame/src/me/nadd/tilegame/Utils.java
+++ TileGame/src/me/nadd/tilegame/Utils.java
@@ -2,15 +2,15 @@ package me.nadd.tilegame;
 
 /**
  * A general utility class
- * @author Drew
- *
+ * @author Kneesnap
  */
 public class Utils {
 	
 	public static int randInt(int min, int max) {
 		return (int) ((Math.random() * (max - min)) + min);
 	}
-        public static int randChoice(){
-            return (Math.random() < 0.5) ? 1 : 0;
+	
+        public static boolean randChoice(){
+            return (Math.random() < 0.5) ? true : false;
         }
 }
