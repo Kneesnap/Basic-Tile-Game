5ba0fe258ee94607475539e028d13592181d76c0
Kneesnap <Kneesnap@users.noreply.github.com>
2017-03-21 10:59:00 -0700

    Commentification Squad Strikes Again.

--- TileGame/src/me/nadd/tilegame/ai/MonsterAI.java
+++ TileGame/src/me/nadd/tilegame/ai/MonsterAI.java
@@ -27,9 +27,12 @@ public abstract class MonsterAI extends AI{
 		Player closest = null;
 		
 		for (Entity check : Core.getEntities()) {
+			//Skip the entity if it is this entity, or it's not a player.
 			if (getEntity() == check || !(check instanceof Player))
 				continue;
+			
 			double dis = getEntity().distanceTo(check);
+			//If the entity we're checking is closer than the current closest entity.
 			if (dis <= minDis){
 				minDis = dis;
 				closest = (Player)check;
@@ -44,6 +47,7 @@ public abstract class MonsterAI extends AI{
 	}
 	
 	protected void defaultAttack() {
+		//If there are no players, the enemies turn into cannibals. Otherwise they attack the player.
 		if (!Core.getLivePlayers().isEmpty())
                     attackNearestPlayer();
                 else
