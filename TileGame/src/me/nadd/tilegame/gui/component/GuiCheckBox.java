be80a3cc6eb2f3b111750f93625087d290940ede
Kneesnap <Kneesnap@users.noreply.github.com>
2017-03-21 09:08:52 -0700

    Update GuiCheckBox.java

--- TileGame/src/me/nadd/tilegame/gui/component/GuiCheckBox.java
+++ TileGame/src/me/nadd/tilegame/gui/component/GuiCheckBox.java
@@ -2,19 +2,37 @@ package me.nadd.tilegame.gui.component;
 
 import me.nadd.tilegame.gui.GUI;
 
-public class GuiCheckBox extends GuiComponent {
-
+/**
+ * A basic checkbox.
+ */
+public class GuiCheckBox extends GUIButton {
+	
+	private boolean state;
+	
 	public GuiCheckBox(int xPos, int yPos) {
-		super(xPos, yPos);
+		super(xPos, yPos, "", 20, this::toggle);
 	}
 
 	@Override
 	public void render(GUI gui) {
-		
+		if(isChecked())
+			gui.setDrawColor(0, 1, 0);
+		else
+			gui.setDrawColor(1, 0, 0);
+		gui.drawRect(getX(), getY(), getX() + getWidth(), getY() + BOX_HEIGHT);
 	}
-
-	@Override
-	public void onMouseClick(GUI gui) {
-		
+	
+	/**
+	 * Toggle the status of this checkbox.
+	 */
+	public void toggle() {
+		this.state = !this.state;
+	}
+	
+	/**
+	 * Is this checkbox checked?
+	 */
+	public boolean isChecked() {
+		return this.state;
 	}
 }
